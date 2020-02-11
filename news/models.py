fc847b318834b172b9c2cb30d31badf250989ac7
mamal anjed <mr.anjedani@gmail.com>
2020-02-11 21:01:53 +0330

    editeds

--- news/models.py
+++ news/models.py
@@ -4,10 +4,16 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 
+class CatNews(models.Model):
+    category = models.CharField( max_length=50)
+
+    def __str__(self):
+        return self.category
+ 
 # Create your models here.
 class News(models.Model):
     title = models.CharField(max_length=100)
-    body = models.TextField(None=False)
+    body = models.TextField(null=False)
     date = models.DateTimeField(auto_now_add=True)
     img = models.ImageField(default='default.png', blank=True)
     author = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
@@ -16,12 +22,5 @@ class News(models.Model):
     def __str__(self):
         return self.title
 
-
     def snippet(self):
         return self.body[:50] + '...'
-
-class CatNews(models.Model):
-    category = models.CharField( max_length=50)
-
-    def __str__(self):
-        return self.title
