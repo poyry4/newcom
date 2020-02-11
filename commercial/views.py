0fbd7e849ae0ef2627ed10946b3e7de51b5458cc
mamal anjed <mr.anjedani@gmail.com>
2020-02-11 23:07:20 +0330

    form in admin is valid and save button is working

--- commercial/views.py
+++ commercial/views.py
@@ -4,6 +4,12 @@ from .form import categorysForm, commercialForm, imageCommercialForm
 
 # Create your views here.
 def adminCommercial_view(request):
-    form = commercialForm()
-    return render(request, 'commercial/adminCommercial.htm', { 'form': form })
+    if request.method == 'POST':
+         form = commercialForm(request.POST)
+         if form.is_valid():
+             form.save()
+             return redirect('commercial:adminCommercial')
+    else:    
+        form = commercialForm()
+        return render(request, 'commercial/adminCommercial.htm', { 'form': form })
 
