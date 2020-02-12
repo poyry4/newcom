1c9f499cad07c5a793179ec607ca492f8179bfc6
pourya <pooryattt@gmail.com>
2020-02-12 22:38:22 +0330

    admin o model o zadim

--- commercial/admin.py
+++ commercial/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import imageCommercial
+from .models import News
+
+admin.site.register(imageCommercial)
+
+
+admin.site.register(commercial)
--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CatNews
+from .models import News
+
+admin.site.register(CatNews)
+
+
+admin.site.register(News)
