faa37b5cb766bc16119ac3ce8a18ad2812b2aa97
pourya <pooryattt@gmail.com>
2020-02-12 22:42:24 +0330

    admin ha dorost shod

--- commercial/admin.py
+++ commercial/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import imageCommercial
-from .models import News
+from .models import commercial, categorys
 
 admin.site.register(imageCommercial)
 
-
 admin.site.register(commercial)
+
+admin.site.register(categorys)
